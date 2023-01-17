e7a3a43bbb11fc20b0cb04cbaf1e81a9b9052c23
Baturpcu <batusalkim11@gmail.com>
2023-01-17 03:05:36 +0300

    last update
    last update

---
+++ C/Soru-1/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//SORU 1
+
+/*
+int main(){
+    int a, b, c, x;
+    printf("a,b,c,x degerlerini giriniz: ");
+    scanf("%d %d %d %d",&a,&b,&c,&x);
+
+    printf("Sonuc : %d", a*x*x + b*x + c );
+
+}
+*/
+//2. SORU
+/*
+int main(){
+    int a,b,h;
+
+    printf("Dikdörtgen prizmanýn a,b ve h uzunluklarini giriniz : \n"  );
+    scanf("%d%d%d",&a,&b,&h);
+
+    int taban_alani = a*b;
+    int yanal_alanlar = 2*((a+b)*h);
+    int toplam_alani = 2*(a*b + a*h + b*h);
+    int hacim = a*b*h;
+
+    printf("taban Alani  %d\n",taban_alani);
+    printf("Yanal alanlar toplami %d\n ",yanal_alanlar);
+    printf("toplam alan %d\n ",toplam_alani);
+    printf("hacmi %d \n",hacim);
+
+}
+*/
+
+//3.SORU
+
+int main(){
+
+}
+
+
